5423a2fcdb868a6e2b041cc35168e8981e8193b0
s110103 <schauer_lukas@yahoo.com>
2020-11-13 17:47:55 +0100

    Initial Commit

---
+++ cScorer_LocalData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cScorer_LocalData
+//
+//  Created by Lukas Schauer on 13.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
