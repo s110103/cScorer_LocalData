e7be653ea98c5448a1cb785036795395084705a8
s110103 <lukasschauer2704@gmail.com>
2020-12-13 09:56:13 +0100

    +fixed touchesEnded Bug
    Fixed Bug which occured when trying to drag a players view

--- cScorer_LocalData/Controller/Match/StartMatchViewController.swift
+++ cScorer_LocalData/Controller/Match/StartMatchViewController.swift
@@ -17,9 +17,7 @@ class StartMatchViewController: UIViewController {
     var selectedIndex: Int = 0
     var currentMatch: Match = Match()
     var delegate: StartMatchViewControllerDelegate?
-    
-    var percentageDrivenInteractiveTransition: UIPercentDrivenInteractiveTransition!
-    
+        
     var isDragging: Bool = false
     var draggingPlayer: String = ""
     var finalLocation: CGPoint = CGPoint(x: 0, y: 0)
@@ -31,6 +29,7 @@ class StartMatchViewController: UIViewController {
     var firstTeamSecondInitialLocation: CGPoint = CGPoint(x: 0, y: 0)
     var secondTeamFirstInitialLocation: CGPoint = CGPoint(x: 0, y: 0)
     var secondTeamSecondInitialLocation: CGPoint = CGPoint(x: 0, y: 0)
+    var serverInitialLocation: CGPoint = CGPoint(x: 0, y: 0)
     
     // MARK: - Outlets
     @IBOutlet weak var firstTeamFirstTopView: UIView!
@@ -68,9 +67,7 @@ class StartMatchViewController: UIViewController {
     @IBOutlet weak var restartButton: UIButton!
     @IBOutlet weak var warmupButton: UIButton!
     @IBOutlet weak var startMatchButton: UIButton!
-    
-    @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
-    
+        
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -81,6 +78,7 @@ class StartMatchViewController: UIViewController {
         firstTeamSecondTopView.isUserInteractionEnabled = true
         secondTeamFirstTopView.isUserInteractionEnabled = true
         firstTeamSecondTopView.isUserInteractionEnabled = true
+        serverView.isUserInteractionEnabled = true
         
         firstTeamFirstInitialLocation.x = firstTeamFirstTopView.frame.origin.x
         firstTeamFirstInitialLocation.y = firstTeamFirstTopView.frame.origin.y
@@ -90,8 +88,8 @@ class StartMatchViewController: UIViewController {
         secondTeamFirstInitialLocation.y = secondTeamFirstTopView.frame.origin.y
         secondTeamSecondInitialLocation.x = secondTeamSecondTopView.frame.origin.x
         secondTeamSecondInitialLocation.y = secondTeamSecondTopView.frame.origin.y
-        
-        addGesture()
+        serverInitialLocation.x = serverView.frame.origin.x
+        serverInitialLocation.y = serverView.frame.origin.y
     }
     
     // MARK: - Actions
@@ -108,46 +106,7 @@ class StartMatchViewController: UIViewController {
     @IBAction func restartButtonTapped(_ sender: UIButton) {
     }
     
-    @IBAction func handlePanGesture(_ panGesture: UIPanGestureRecognizer) {
-        if isDragging == false {
-            let percent = max(panGesture.translation(in: view).x, 0) / view.frame.width
-            
-            switch panGesture.state {
-            case .began:
-                navigationController?.delegate = self
-                navigationController?.popViewController(animated: true)
-                delegate?.sendStartMatchData(currentMatch: currentMatch, selectedIndex: selectedIndex)
-                
-            case .changed:
-                percentageDrivenInteractiveTransition.update(percent)
-                
-            case .ended:
-                let velocity = panGesture.velocity(in: view).x
-                    
-                if percent > 0.5 || velocity > 1000 {
-                    percentageDrivenInteractiveTransition.finish()
-                } else {
-                    percentageDrivenInteractiveTransition.cancel()
-                }
-                
-            case .cancelled, .failed:
-                percentageDrivenInteractiveTransition.cancel()
-                
-            default:
-                break
-            }
-        }
-    }
-    
     // MARK: - Functions
-    func addGesture() {
-        guard (navigationController?.viewControllers.count)! > 1 else {
-            return
-        }
-        
-        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(AddMatchViewController.handlePanGesture(_:)))
-        self.view.addGestureRecognizer(panGestureRecognizer)
-    }
 
 }
 
@@ -163,169 +122,119 @@ extension StartMatchViewController {
                 
         let touchedLocation = touch.location(in: view)
         
-        let location = touch.location(in: firstTeamFirstTopView)
-        xOffset = location.x
-        yOffset = location.y
-        
-        if firstTeamFirstTopView.bounds.contains(location) {
-            isDragging = true
-            finalLocation = touchedLocation
-        }
-        
-        /*
-         
-         if touchedLocation.x >= firstTeamFirstTopView.frame.minX && touchedLocation.x <= firstTeamFirstTopView.frame.maxX && touchedLocation.y >= firstTeamFirstTopView.frame.minY && touchedLocation.y <= firstTeamFirstTopView.frame.maxY {
-             
-             print("First Player First View")
-             
-             let location = touch.location(in: firstTeamFirstTopView)
-             xOffset = location.x
-             yOffset = location.y
-             
-             
-             
-             
-             if firstTeamFirstTopView.bounds.contains(location) {
-                 isDragging = true
-                 finalLocation = touchedLocation
-                 currentView = firstTeamFirstTopView
-             }
-         }
-         
-         else if touchedLocation.x >= firstTeamSecondTopView.frame.minX && touchedLocation.x <= firstTeamSecondTopView.frame.maxX && touchedLocation.y >= firstTeamSecondTopView.frame.minY && touchedLocation.y <= firstTeamSecondTopView.frame.maxY {
-            print("First Player Second View")
+        if touchedLocation.x >= firstTeamFirstTopView.frame.minX && touchedLocation.x <= firstTeamFirstTopView.frame.maxX && touchedLocation.y >= firstTeamFirstTopView.frame.minY && touchedLocation.y <= firstTeamFirstTopView.frame.maxY {
+            
+            let location = touch.location(in: firstTeamFirstTopView)
+            xOffset = location.x
+            yOffset = location.y
+                    
+            if firstTeamFirstTopView.bounds.contains(location) {
+                isDragging = true
+                finalLocation = touchedLocation
+                currentView = firstTeamFirstTopView
+            }
+        }else if touchedLocation.x >= firstTeamSecondTopView.frame.minX && touchedLocation.x <= firstTeamSecondTopView.frame.maxX && touchedLocation.y >= firstTeamSecondTopView.frame.minY && touchedLocation.y <= firstTeamSecondTopView.frame.maxY {
             
             let location = touch.location(in: firstTeamSecondTopView)
             xOffset = location.x
             yOffset = location.y
             finalLocation = touchedLocation
             
-            isDragging = true
-            currentView = firstTeamSecondTopView
+            if firstTeamFirstTopView.bounds.contains(location) {
+                isDragging = true
+                finalLocation = touchedLocation
+                currentView = firstTeamSecondTopView
+            }
+            
         } else if touchedLocation.x >= secondTeamFirstTopView.frame.minX && touchedLocation.x <= secondTeamFirstTopView.frame.maxX && touchedLocation.y >= secondTeamFirstTopView.frame.minY && touchedLocation.y <= secondTeamFirstTopView.frame.maxY {
-            print("Second Player First View")
             
             let location = touch.location(in: secondTeamFirstTopView)
             xOffset = location.x
             yOffset = location.y
             finalLocation = touchedLocation
             
-            isDragging = true
-            currentView = secondTeamFirstTopView
+            if secondTeamFirstTopView.bounds.contains(location) {
+                isDragging = true
+                finalLocation = touchedLocation
+                currentView = secondTeamFirstTopView
+            }
         } else if touchedLocation.x >= secondTeamSecondTopView.frame.minX && touchedLocation.x <= secondTeamSecondTopView.frame.maxX && touchedLocation.y >= secondTeamSecondTopView.frame.minY && touchedLocation.y <= secondTeamSecondTopView.frame.maxY {
-            print("Second Player Second View")
             
             let location = touch.location(in: secondTeamSecondTopView)
             xOffset = location.x
             yOffset = location.y
             finalLocation = touchedLocation
             
-            isDragging = true
-            currentView = secondTeamSecondTopView
+            if secondTeamSecondTopView.bounds.contains(location) {
+                isDragging = true
+                finalLocation = touchedLocation
+                currentView = secondTeamSecondTopView
+            }
         } else if touchedLocation.x >= serverView.frame.minX && touchedLocation.x <= serverView.frame.maxX && touchedLocation.y >= serverView.frame.minY && touchedLocation.y <= serverView.frame.maxY {
-            print("Server View")
             
             let location = touch.location(in: serverView)
             xOffset = location.x
             yOffset = location.y
             finalLocation = touchedLocation
             
-            isDragging = true
-            currentView = serverView
+            if serverView.bounds.contains(location) {
+                isDragging = true
+                finalLocation = touchedLocation
+                currentView = serverView
+            }
         }
-         */
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard isDragging, let touch = touches.first else {
+        guard isDragging, let touch = touches.first, currentView != nil else {
             return
         }
-        
+                
         let location = touch.location(in: view)
-        firstTeamFirstTopView.frame.origin.x = location.x - xOffset
-        firstTeamFirstTopView.frame.origin.y = location.y - yOffset
+        currentView!.frame.origin.x = location.x - xOffset
+        currentView!.frame.origin.y = location.y - yOffset
         
         finalLocation = location
-        
-        /*
-         if currentView != nil {
-             print("not nil")
-             let location = touch.location(in: view)
-             currentView?.frame.origin.x = location.x - xOffset
-             currentView?.frame.origin.y = location.y - yOffset
-             
-             finalLocation = location
-         } else {
-             print("nil")
-         }
-         */
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        
-        if isDragging == true {
+        if isDragging == true && currentView != nil {
             isDragging = false
             
             if firstTeamFirstTargetView.frame.contains(finalLocation) {
-                firstTeamFirstTopView.frame.origin.x = firstTeamSecondTargetView.frame.origin.x
-                firstTeamFirstTopView.frame.origin.y = firstTeamSecondTargetView.frame.origin.y
+                currentView!.frame.origin.x = firstTeamFirstTargetView.frame.origin.x
+                currentView!.frame.origin.y = firstTeamFirstTargetView.frame.origin.y
+            } else if firstTeamSecondTargetView.frame.contains(finalLocation) {
+                currentView!.frame.origin.x = firstTeamSecondTargetView.frame.origin.x
+                currentView!.frame.origin.y = firstTeamSecondTargetView.frame.origin.y
+            } else if secondTeamFirstTargetView.frame.contains(finalLocation) {
+                currentView!.frame.origin.x = secondTeamFirstTargetView.frame.origin.x
+                currentView!.frame.origin.y = secondTeamFirstTargetView.frame.origin.y
+            } else if secondTeamSecondTargetView.frame.contains(finalLocation) {
+                currentView!.frame.origin.x = secondTeamSecondTargetView.frame.origin.x
+                currentView!.frame.origin.y = secondTeamSecondTargetView.frame.origin.y
             } else {
-                if secondTeamFirstTargetView.frame.contains(finalLocation) {
-                    firstTeamFirstTopView.frame.origin.x = secondTeamFirstTargetView.frame.origin.x
-                    firstTeamFirstTopView.frame.origin.y = secondTeamFirstTargetView.frame.origin.y
-                } else {
+                
+                
+                if currentView == firstTeamFirstTopView {
                     firstTeamFirstTopView.frame.origin.x = firstTeamFirstInitialLocation.x
                     firstTeamFirstTopView.frame.origin.y = firstTeamFirstInitialLocation.y
+                } else if currentView == firstTeamSecondTopView {
+                    firstTeamSecondTopView.frame.origin.x = firstTeamSecondInitialLocation.x
+                    firstTeamSecondTopView.frame.origin.y = firstTeamSecondInitialLocation.y
+                } else if currentView == secondTeamFirstTopView {
+                    secondTeamFirstTopView.frame.origin.x = secondTeamFirstInitialLocation.x
+                    secondTeamFirstTopView.frame.origin.y = secondTeamFirstInitialLocation.y
+                } else if currentView == secondTeamSecondTopView {
+                    secondTeamSecondTopView.frame.origin.x = secondTeamSecondInitialLocation.x
+                    secondTeamSecondTopView.frame.origin.y = secondTeamSecondInitialLocation.y
+                } else if currentView == serverView {
+                    serverView.frame.origin.x = serverInitialLocation.x
+                    serverView.frame.origin.y = serverInitialLocation.y
                 }
+                currentView = nil
             }
         }
-        
-        
-        /*
-         
-         if isDragging == true {
-             isDragging = false
-             
-             if firstTeamFirstTargetView.frame.contains(finalLocation) {
-                 currentView?.frame.origin.x = firstTeamFirstTargetView.frame.origin.x
-                 currentView?.frame.origin.y = firstTeamFirstTargetView.frame.origin.y
-             }
-         }
-         
-         else if firstTeamSecondTargetView.frame.contains(finalLocation) {
-            currentView?.frame.origin.x = firstTeamSecondTargetView.frame.origin.x
-            currentView?.frame.origin.y = firstTeamSecondTargetView.frame.origin.y
-        } else if secondTeamFirstTargetView.frame.contains(finalLocation) {
-            currentView?.frame.origin.x = secondTeamFirstTargetView.frame.origin.x
-            currentView?.frame.origin.y = secondTeamFirstTargetView.frame.origin.y
-        } else if secondTeamSecondTargetView.frame.contains(finalLocation) {
-            currentView?.frame.origin.x = secondTeamSecondTargetView.frame.origin.x
-            currentView?.frame.origin.y = secondTeamSecondTargetView.frame.origin.y
-        } else {
-            
-        }
-         */
-        
-    }
-}
-
-extension StartMatchViewController: UINavigationControllerDelegate {
-    
-    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return SlideAnimatedTransitioning()
-    }
-    
-    func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        navigationController.delegate = nil
-        
-        if panGestureRecognizer.state == .began {
-            percentageDrivenInteractiveTransition = UIPercentDrivenInteractiveTransition()
-            percentageDrivenInteractiveTransition.completionCurve = .easeOut
-        } else {
-            percentageDrivenInteractiveTransition = nil
-        }
-        
-        return percentageDrivenInteractiveTransition
     }
 }
